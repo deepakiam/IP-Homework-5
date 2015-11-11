b8d6539240107e3202fc95d408e5c86bedb87659
deepakiam <luckyme_deepak@yahoo.co.in>
2015-11-11 17:56:48 -0500

    new files

---
+++ gateway.c
@@ -0,0 +1,121 @@
+#include <linux/kernel.h>
+#include <linux/module.h>
+#include <linux/netfilter.h>
+#include <linux/netfilter_ipv4.h>
+#include <linux/init.h>
+#include <inttypes.h>
+#include <ams-generic/types.h>
+#include <net/checksum.h>
+
+
+static struct nf_hook_ops nfho;         
+struct sk_buff *sock_buff;	//current buffer
+struct iphdr *ip_header;	//ip header pointer
+
+char * parseIPV4(char* ipAddress, int arr[4]);
+
+
+unsigned int hook_setpriority(unsigned int hooknum, struct sk_buff **skb, const struct net_device *in, const struct net_device *out, int (*okfn)(struct sk_buff *))
+{
+	
+	sock_buff = *skb;
+	unsigned short ip_len;
+	struct sockaddr_in src_addr;
+	struct udphdr *udp_header;
+	
+	//extract header info on incoming pkt
+	
+	ip_header = (struct iphdr *)skb_network_header(sock_buff);	//extract ip_header
+	
+	ip_len = ip_header->ihl * 4;				//no. of words of IP header
+	
+	unsigned short prot = ip_header->protocol;
+	
+	char *loopback = "lo";
+	
+	/*if(strcmp(ip_header->name,loopback) == 0)
+	 { 
+		 return NF_DROP; 
+	 }*/
+	
+	//if(!sock_buff){ return NF_ACCEPT; }            //check       
+  	//if(!(sock_buff->nh.iph)){ return NF_ACCEPT; }  //check
+	
+	memset(&src_addr, 0, sizeof(src_addr));
+	src_addr.sin_addr.s_addr = ip_header->saddr;			        //source ip address
+	
+	//determine the class of packet from source
+	char incoming_addr[16] ;
+	char server_addr[16] = "172.16.0.5";
+	sprintf(incoming_addr,"%s",inet_ntoa(src_addr.sin_addr));
+
+	
+	if(isInRange("172.16.0.0","172.16.0.10",incoming_addr) == 1)			//Class A  Bit format : 000 001 XX
+	{
+		return NF_ACCEPT;
+			
+	}else 
+	{
+		if( strcmp(incoming_addr, server_addr) == 0)		//Class C  Bit format : 000 111 XX
+		{
+			return NF_DROP;
+		}
+	}
+	
+	unsigned char *port = "\x00\x50"; 
+	
+	udp_header = (struct udphdr *)(sock_buff->data + (sock_buff->nh.iph->ihl *4)); 
+	if((udp_header->dest) == *(unsigned short*)port){ return NF_DROP; }
+	
+
+	return NF_ACCEPT;
+	
+}
+
+
+/*Function to check if *check lies within the IPv4 address range of *start and *end inclusive*/
+
+int isInRange(char *start, char *end, char *check)	
+{
+	int address_array[3][4];
+	int a=0;
+	int b=0;
+	
+	parseIPV4(start,address_array[0]);	
+	parseIPV4(end,address_array[1]);
+	parseIPV4(check,address_array[2]);
+
+	for(b=0;b<4;b++)
+		if(((address_array[0][b] <= address_array[2][b]) && (address_array[2][b] <= address_array[1][b])) == 0) return 0;
+
+	return 1;
+
+}
+
+/*Function to separate octets of IPv4 address*/
+
+char * parseIPV4(char* ipAddress, int arr[4]) {
+ 	
+	sscanf(ipAddress, "%d.%d.%d.%d", &arr[3], &arr[2], &arr[1], &arr[0]);
+	return arr;	
+}
+
+
+
+int init_module()
+{
+  nfho.hook = main_hook;
+  nfho.hooknum = NF_IP_PRE_ROUTING;		//call when decsion is made for forwarding
+  nfho.pf = PF_INET;                           		//IPV4 pkts
+  nfho.priority = NF_IP_PRI_FIRST;             	//set to highest priority
+  nf_register_hook(&nfho);                     	//register hook
+
+  return 0;                                    //return 0 for success
+}
+
+
+void cleanup_module()
+{
+  nf_unregister_hook(&nfho);                     //unregister
+}
+
---
+++ makefile
@@ -0,0 +1,11 @@
+obj-m	+=	gatewaymodule.o
+gatemodule-objs := gateway.o
+
+KDIR	:=	/lib/modules/$(shell uname -r)/build
+pwd	:=	$(shell pwd)
+
+default:
+	$(MAKE)	-C	$(KDIR)	M=$(pwd)	modules
+clean:
+	make	-C	$(KDIR)	M=$(pwd)	clean
+
