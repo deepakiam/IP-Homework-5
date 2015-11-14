613eeeed390d7c26997fa6929e205cf923c57a11
deepakiam <luckyme_deepak@yahoo.co.in>
2015-11-14 17:45:42 -0500

    changed

--- firewall.c
+++ firewall.c
@@ -36,21 +36,23 @@ unsigned int main_hook(unsigned int hooknum,
 {
 	if(strcmp(in->name,allow) == 0){ return NF_ACCEPT; }
 	if(strcmp(in->name,internal) == 0){ return NF_ACCEPT; }
-  if(strcmp(in->name,interface) == 0){ return NF_DROP; }     
-        
+  if(strcmp(in->name,interface) == 0){ return NF_DROP; }    
+        printk(KERN_INFO "in hook function\n");
   sock_buff = *skb;
+	ip_header = (struct iphdr *)skb_network_header(sock_buff);	//extract ip_header
   if(!sock_buff){ return NF_ACCEPT; }                   
   if(!(sock_buff->nh.iph)){ return NF_ACCEPT; }              
   //if(sock_buff->nh.iph->saddr == *(unsigned int*)ip_address){ return NF_DROP; }
                 
-  
-if(sock_buff->nh.iph->protocol != 17){ return NF_ACCEPT; }                 
-udp_header = (struct udphdr *)(sock_buff->data + (sock_buff->nh.iph->ihl *4)); 
+  printk(KERN_INFO "a legit packet\n");
+if(ip_header->protocol != 17){ return NF_ACCEPT; }                 
+udp_header = (struct udphdr *)(sock_buff->data + (ip_header->ihl *4)); 
 if((udp_header->dest) == *(unsigned short*)port){ return NF_DROP; }
 return NF_ACCEPT;
 }
 int init_module()
 {
+		printk(KERN_INFO "initialize kernel module\n");
         netfilter_ops.hook              =       main_hook;
         netfilter_ops.pf                =       PF_INET;        
         netfilter_ops.hooknum           =       NF_IP_PRE_ROUTING;
